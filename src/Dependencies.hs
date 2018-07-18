a4e14dfa85438f28f19dc2b1b743e0a9d9dd6a9f
Georges Dubus <georges.dubus@soundcloud.com>
2018-07-18 19:48:46 +0200

    Move getModInfo to ModPortal.hs

--- src/Dependencies.hs
+++ src/Dependencies.hs
@@ -1,11 +1,7 @@
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE FlexibleInstances #-}
 module Dependencies
   ( resolveDeps
   , flattenDeps
@@ -18,46 +14,10 @@ import ModPortal
 
 import qualified RIO.Set as Set
 import qualified RIO.Map as Map
-import qualified RIO.Map.Partial as Map
 import qualified RIO.List as List
-import qualified RIO.Text as Text
-import RIO.FilePath
 
-import Data.Aeson
 import Data.Fix
-import Codec.Archive.Zip
-import Lens.Micro.TH
-import Data.Aeson.TH
 import Data.Aeson.Yak
-import qualified Text.Regex.Applicative.Text as Regex
-
-data Dependency = Dependency
-  { dependencyOption :: Bool
-  , dependencyName :: Text
-  , dependencyContraint :: Maybe Text
-  } deriving (Show)
-makeFields ''Dependency
-
-instance FromJSON Dependency where
-  parseJSON = withText "dependency" $ \s -> maybe (fail ("Could not parse " <> show s)) pure (Regex.match regex s)
-      where regex = Dependency <$> optionR <*> nameR <*> constraintR
-            optionR = isJust <$> optional (Regex.string "? ")
-            nameR = Text.pack <$> some (Regex.psym (/= ' '))
-            constraintR = optional $ Text.pack <$ Regex.string " >= " <*> some Regex.anySym
-
-
-data ModInfo = ModInfo {modInfoName :: Text, modInfoVersion :: Text, modInfoFactorioVersion :: Text, modInfoDependencies :: Yak Dependency} deriving (Show)
-makeFields ''ModInfo
-deriveFromJSON defaultOptions { fieldLabelModifier = camelTo2 '_' . drop (Text.length "modInfo")} '' ModInfo
-
-getModInfo :: Mod a -> RIO App ModInfo
-getModInfo mod_ = do
-  modFile <- fetchMod mod_
-  infoEntry <- withArchive modFile $ do
-    s <- mkEntrySelector (dropExtension (modFilename mod_) </> "info.json")
-    getEntry s
-
-  either fail return (eitherDecodeStrict infoEntry)
 
 lookupDependencies :: Mod a -> RIO App (Mod Text)
 lookupDependencies mod_ = do
--- src/ModPortal.hs
+++ src/ModPortal.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NoImplicitPrelude #-}
@@ -5,6 +9,9 @@
 module ModPortal
   ( getMods
   , fetchMod
+  , Dependency(..), name, option, constraint
+  , ModInfo (..), version, dependencies
+  , getModInfo
   ) where
 
 import Import
@@ -14,13 +21,17 @@ import RIO.Directory
 import RIO.FilePath
 import qualified RIO.ByteString.Lazy as BL
 import qualified RIO.Map.Unchecked as Map
-
+import qualified RIO.Text as Text
 
 import Data.Aeson
 import Data.Aeson.Types (Parser, parseEither)
 import Network.HTTP.Simple
 import Network.HTTP.Conduit (parseUrlThrow)
-
+import Lens.Micro.TH
+import Data.Aeson.TH
+import Data.Aeson.Yak
+import qualified Text.Regex.Applicative.Text as Regex
+import Codec.Archive.Zip
 
 getMods :: RIO App (Map Text (Mod Text))
 getMods = do
@@ -64,3 +75,37 @@ fetchMod Mod {modFilename, modDownloadUrl} = do
       $ req
     writeFileBinary modFile (getResponseBody response)
   return modFile
+
+
+data Dependency = Dependency
+  { dependencyOption :: Bool
+  , dependencyName :: Text
+  , dependencyConstraint :: Maybe Text
+  } deriving (Show)
+makeFields ''Dependency
+
+instance FromJSON Dependency where
+  parseJSON = withText "dependency" $ \s -> maybe (fail ("Could not parse " <> show s)) pure (Regex.match regex s)
+      where regex = Dependency <$> optionR <*> nameR <*> constraintR
+            optionR = isJust <$> optional (Regex.string "? ")
+            nameR = Text.pack <$> some (Regex.psym (/= ' '))
+            constraintR = optional $ Text.pack <$ Regex.string " >= " <*> some Regex.anySym
+
+
+data ModInfo = ModInfo
+  { modInfoName :: Text
+  , modInfoVersion :: Text
+  , modInfoFactorioVersion :: Text
+  , modInfoDependencies :: Yak Dependency
+  } deriving (Show)
+makeFields ''ModInfo
+deriveFromJSON defaultOptions { fieldLabelModifier = camelTo2 '_' . drop (Text.length "modInfo")} '' ModInfo
+
+getModInfo :: Mod a -> RIO App ModInfo
+getModInfo mod_ = do
+  modFile <- fetchMod mod_
+  infoEntry <- withArchive modFile $ do
+    s <- mkEntrySelector (dropExtension (modFilename mod_) </> "info.json")
+    getEntry s
+
+  either fail return (eitherDecodeStrict infoEntry)
